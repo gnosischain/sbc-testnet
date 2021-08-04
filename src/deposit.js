02718a4245486526221ba576432e51139a439555
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-08-04 10:49:31 +0400

    Sbc test docker compose

--- src/deposit.js
+++ src/deposit.js
@@ -2,7 +2,7 @@ const Web3 = require('web3')
 
 const { abi } = require('./artifacts/contract.json')
 
-const { RPC_URL, DEPLOYMENT_PRIVATE_KEY, BATCH_SIZE } = process.env
+const { RPC_URL, DEPLOYMENT_PRIVATE_KEY, BATCH_SIZE, N, OFFSET } = process.env
 
 const web3 = new Web3(RPC_URL)
 const { address } = web3.eth.accounts.wallet.add(DEPLOYMENT_PRIVATE_KEY)
@@ -10,14 +10,17 @@ const { address } = web3.eth.accounts.wallet.add(DEPLOYMENT_PRIVATE_KEY)
 const depositData = require('/tmp/deposits.json')
 
 const batchSize = parseInt(BATCH_SIZE, 10)
+const offset = parseInt(OFFSET, 10)
+const n = parseInt(N, 10)
 async function main() {
   const contract = new web3.eth.Contract(abi, process.argv[2])
+  const deposits = depositData.slice(offset, offset + n)
 
-  console.log(`Sending ${Math.ceil(depositData.length / batchSize)} deposit transactions in batches of ${batchSize} events`)
+  console.log(`Sending ${Math.ceil(deposits.length / batchSize)} deposit transactions for ${deposits.length} deposits in batches of ${batchSize} events`)
   let nonce = await web3.eth.getTransactionCount(address)
   let count = 0
   let arr = [[], [], [], []]
-  for (const deposit of depositData) {
+  for (const deposit of deposits) {
     arr[0].push(`0x${deposit.pubkey}`)
     arr[1].push(`0x${deposit.withdrawal_credentials}`)
     arr[2].push(`0x${deposit.signature}`)
